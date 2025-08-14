b3adce3849776fbdc3644c8b2b46a6aaea8d9c6f
Rehaan Rahman <RehaanDjango@Rehaans-MacBook-Pro.local>
2025-08-14 15:39:36 +1000

    first group commit, did create, read, update, delete together

---
+++ a_incidents/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, redirect
+from a_incidents.models import Incident
+from .forms import IncidentForm
+
+# Create your views here.
+
+def home_page(request):
+
+
+    incidents = Incident.objects.all()
+
+    return render(request, 'a_incidents/home.html', {
+        'incidents': incidents
+    })
+
+
+def create_incident(request):
+    if request.method == 'POST':
+        form = IncidentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # Redirect to a list or detail page
+    else:
+        form = IncidentForm()
+
+    return render(request, 'a_incidents/create_incident.html', {'form': form})
+
+
+
+# views.py
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Incident
+
+def delete_incident(request, pk):
+
+    incident = get_object_or_404(Incident, pk=pk)
+
+    if request.method == "POST":  # User confirmed deletion
+        incident.delete()
+        return redirect('home')  # Go back to list after deleting
+
+    # Show confirmation page
+    return render(request, 'a_incidents/confirm_delete.html', {'incident': incident})
+
+
+def update_incident(request, pk):
+
+    incident = get_object_or_404(Incident, pk=pk)
+
+    if request.method == "POST":
+        form = IncidentForm(request.POST, instance=incident)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = IncidentForm(instance=incident)
+
+    return render(request, 'a_incidents/incident_form.html', {'form': form})
