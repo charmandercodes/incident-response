eafc92e648f7cd9b51941bb00bd28d8b6efe5546
Boris Tran <boris.tran@student.uts.edu.au>
2025-10-01 20:59:06 +1000

    Update urls.py

--- a_reports/urls.py
+++ a_reports/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import CombinedIncidentsReportView, EnhancedIncidentReportView
+from .views import combined_incident_report, enhanced_incident_report
 
 urlpatterns = [
-    path("combined/", CombinedIncidentsReportView.as_view(), name="combined_incident_report"),
-    path("incident/<int:pk>/enhanced/", EnhancedIncidentReportView.as_view(), name="enhanced_incident_report"),
+    path("combined/", combined_incident_report, name="combined_incident_report"),
+    path("incident/<int:pk>/", enhanced_incident_report, name="enhanced_incident_report"),
 ]
